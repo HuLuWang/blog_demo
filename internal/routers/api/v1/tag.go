faf4b4a388b36fa4def5628a0ec7721adefd136a
汪先森 <whj13004@gmail.com>
2020-09-30 14:29:44 +0800

    validate

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/HuLuWang/blog_demo/global"
+	"github.com/HuLuWang/blog_demo/pkg/app"
+	"github.com/HuLuWang/blog_demo/pkg/errcode"
+	"github.com/gin-gonic/gin"
+)
 
 type Tag struct{}
 
@@ -26,7 +31,21 @@ func (t Tag) Create(c *gin.Context) {}
 // @Param page_size query int false "条数"
 // @Success 200
 // @Router /api/v1/tags [get]
-func (t Tag) List(c *gin.Context) {}
+func (t Tag) List(c *gin.Context) {
+	param := struct {
+		Name  string `form:"name" binding:"max=100"`
+		State uint8  `form:"state,default=1" binding:"oneof= 0 1"`
+	}{}
+	response := app.NewResponse(c)
+	valid, errs := app.BindAndValid(c, &param)
+	if valid == true {
+		global.Logger.Errorf(c, "app.BindAndValid errs: %v", errs)
+		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
+		return
+	}
+	response.ToResponse(gin.H{})
+	return
+}
 
 // @Summary 更新
 // @Tags Tag 标签
@@ -39,7 +58,6 @@ func (t Tag) List(c *gin.Context) {}
 // @Success 200
 // @Router /api/v1/tags/{id} [put]
 func (t Tag) Update(c *gin.Context) {}
-
 
 // @Summary 删除
 // @Tags Tag 标签
