9426911e3c27ed8a25a9c226df3bbb97ad28aa46
汪先森 <whj13004@gmail.com>
2020-10-01 23:00:08 +0800

    人对自己好才是真的

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,7 +1,40 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Tag struct {
 	*Model
 	Name  string
 	State uint8
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	// 必需指定model
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return  nil, err
+	}
+	return tags, nil
+	
+	
 }
